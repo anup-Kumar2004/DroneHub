045fc9d9b9609e616d59c1f49fe6d6e1a6b71fbb
Anup Kumar <anup20102001@gmail.com>
2024-01-09 16:25:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('instagram-icon').href = 'https://instagram.com/dronehub.bml?igshid=OGQ5ZDc2ODk2ZA==';
+document.getElementById('facebook-icon').href = 'https://www.facebook.com/profile.php?id=61554400882970&mibextid=2JQ9oc';
+document.getElementById('youtube-icon').href = 'https://www.youtube.com/your-youtube';
+document.getElementById('linkedin-icon').href = 'https://www.linkedin.com/in/your-linkedin';
+document.getElementById('twitter-icon').href = 'https://twitter.com/your-twitter';
+
